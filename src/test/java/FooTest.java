9d5ea380bc57dd9c7800cab262b87df4593c2386
Fredrik Wallgren <pornoctd@hotmail.com>
2020-03-21 14:57:05 +0100

    ny unit

--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -1,10 +1,9 @@
-import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FooTest {
 
-    @Test
+    @org.junit.jupiter.api.Test
     void method() {
         assertEquals(true, Foo.myMethod());
     }
