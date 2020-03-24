e4010cd59c9d2fc9d161beb51e87116642045820
Fredrik Wallgren <pornoctd@hotmail.com>
2020-03-24 21:58:09 +0100

    ny test

---
+++ src/main/java/BigClass.java
@@ -0,0 +1,64 @@
+public class BigClass {
+    private int number;
+    private String text;
+
+    public BigClass(int number, String text) {
+        this.number = number;
+        this.text = text;
+    }
+
+    public BigClass (int number) {
+        this(number, null);
+    }
+
+    public BigClass(String text) {
+        this(0, text);
+    }
+
+    public BigClass() {
+        this(0, null);
+
+    }
+
+    public void setNumber(int number) {
+        if (number > 0) {
+            this.number = number;
+        } else {
+            this.number = 0;
+        }
+    }
+
+    public int getNumber () {
+        return number;
+    }
+
+    public void setText (String text) {
+        this.text = text;
+    }
+
+    public String getText () {
+        return text;
+    }
+
+    public String textToUpperCase (String text) {
+        return text.toUpperCase();
+    }
+
+    public String textToNullMethod () {
+        this.text = null;
+        return null;
+    }
+
+    public void sumOfNumber (int numberTwo) {
+        if (numberTwo > 0) {
+            this.number = this.number + numberTwo;
+        } else {
+            return;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return  "number=" + number + ", text=" + text ;
+    }
+}
---
+++ src/test/java/BigClassTest.java
@@ -0,0 +1,73 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BigClassTest {
+
+    @Test
+    void classTest() {
+        BigClass b1 = new BigClass();
+        BigClass b2 = new BigClass();
+          assertEquals(b1.getNumber(), b2.getNumber());
+          assertEquals(b1.getText(), b2.getText());
+    }
+
+    @Test
+    void checkNumber() {
+        BigClass b1 = new BigClass(69);
+        assertEquals(69, b1.getNumber());
+    }
+
+    @Test
+    void checkTextAndNumber() {
+        BigClass b1 = new BigClass("Hejsan");
+        assertEquals("Hejsan", b1.getText());
+    }
+
+    @Test
+    void doubleWhammy () {
+        BigClass b1 = new BigClass(69, "Hejsan");
+        assertEquals(69, b1.getNumber());
+        assertEquals("Hejsan", b1.getText());
+    }
+
+    @Test
+    void negativeNumber() {
+        BigClass b1 = new BigClass();
+        b1.setNumber(-5);
+        assertEquals(0, b1.getNumber());
+    }
+
+    @Test
+    void testText () {
+        BigClass b1 = new BigClass();
+        b1.setText("Hej");
+        assertEquals("Hej", b1.getText());
+    }
+
+    @Test
+    void upperCase() {
+        BigClass b1 = new BigClass();
+        assertEquals("JAAA", b1.textToUpperCase("jaaa"));
+    }
+
+    @Test
+    void textNullText() {
+        BigClass b1 = new BigClass("hej");
+        assertEquals(null, b1.textToNullMethod());
+    }
+
+    @Test
+    void numberAgain() {
+        BigClass b1 = new BigClass(6);
+        big.sumOfNumber(6);
+        assertEquals(12, b1.getNumber());
+    }
+
+    @Test
+    void stringTextTest() {
+        BigClass b1 = new BigClass(69, "hej");
+        assertEquals("number=69, text=hej", big.toString());
+    }
+
+}
